6f98bdc7ff63fed21ff4b086345d57b0a9abdbd4
Vineel Sai <mail@vineelsai.com>
2023-01-14 22:28:11 +0530

    terraform deploy lambda

---
+++ Terraform/create-aws-lambda/main.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "shortify.zip"
+  function_name = "shortify"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "app.handler"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("shortify.zip")
+
+  runtime       = "nodejs16.x"
+  architectures = ["x86_64"]
+}
+
+resource "aws_apigatewayv2_api" "example" {
+  name          = "ServerlessExample"
+  description   = "Terraform Serverless Application Example"
+  protocol_type = "HTTP"
+  target = aws_lambda_function.test_lambda.arn
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_apigatewayv2_api.example.execution_arn}/*/*"
+}
