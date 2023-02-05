090f44e14920fb9a434a8914b6d7a6b891546263
Vineel Sai <mail@vineelsai.com>
2023-02-05 21:52:57 +0530

    zip from ts

--- GitHub Backup/src/index.ts
+++ GitHub Backup/src/index.ts
@@ -2,20 +2,21 @@ import { Octokit } from "octokit"
 import { exec } from "child_process"
 import fs from "fs"
 import path from "path"
+import AdmZip from 'adm-zip'
 import "dotenv/config"
 
 const octokit = new Octokit({ auth: process.env.AUTH_TOKEN })
 const baseDir = process.env.BASE_DIR!
 
-interface OrganizationData {
-	organization: {
-		repositories: {
-			nodes: Array<{
-				url: string
-			}>
-		}
-	}
-}
+// interface OrganizationData {
+// 	organization: {
+// 		repositories: {
+// 			nodes: Array<{
+// 				url: string
+// 			}>
+// 		}
+// 	}
+// }
 
 interface UserData {
 	user: {
@@ -82,18 +83,30 @@ const cloneRepo = (url: string) => {
 // 	})
 // }
 
-for (const user of users) {
-	const userData: UserData = await octokit.graphql(`query {
-		user(login: "${user}") {
-			repositories(first: 100 ownerAffiliations: OWNER) {
-				nodes {
-					url
+const run = async () => {
+	for await (const user of users) {
+		const userData: UserData = await octokit.graphql(`query {
+			user(login: "${user}") {
+				repositories(first: 100 ownerAffiliations: OWNER) {
+					nodes {
+						url
+					}
 				}
 			}
-		}
-	}`)
+		}`)
 
-	userData.user.repositories.nodes.map((repo) => {
-		cloneRepo(repo.url)
-	})
+		userData.user.repositories.nodes.map((repo) => {
+			cloneRepo(repo.url)
+		})
+	}
+}
+
+await run()
+
+try {
+	const zip = new AdmZip()
+	zip.addLocalFolder(baseDir)
+	zip.writeZip("repos.zip")
+} catch (err) {
+	console.error(err)
 }
