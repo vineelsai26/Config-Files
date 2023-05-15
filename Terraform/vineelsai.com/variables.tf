13b321de5e6d20415aa01f5453d44937fbf58978
Vineel Sai <mail@vineelsai.com>
2023-05-15 22:11:44 +0530

    delete Stacks

---
+++ Terraform/vineelsai.com/variables.tf
@@ -0,0 +1,9 @@
+variable "cloudflare_api_token" {
+  description = "value of CLOUDFLARE_API_TOKEN"
+  type        = string
+}
+
+variable "cloudflare_zone_id" {
+  description = "value of CLOUDFLARE_ZONE_ID"
+  type        = string
+}
---
+++ Terraform/vineelsai.com/zone.tf
@@ -0,0 +1,7 @@
+resource "cloudflare_zone" "terraform_managed_resource_774c57de3172a6220c23461a2992c3a8" {
+  account_id = "bdd95b5abd61d4c1ee94ec8a0f6a60cf"
+  paused     = false
+  plan       = "free"
+  type       = "full"
+  zone       = "vineelsai.com"
+}
