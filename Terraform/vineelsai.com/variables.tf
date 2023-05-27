c1f3414f3889de86d31c0033640229b64bc93068
Vineel Sai <mail@vineelsai.com>
2023-05-27 21:54:33 +0530

    migrate vineelsai.com from here

--- Terraform/vineelsai.com/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cloudflare_api_token" {
-  description = "value of CLOUDFLARE_API_TOKEN"
-  type        = string
-}
-
-variable "cloudflare_zone_id" {
-  description = "value of CLOUDFLARE_ZONE_ID"
-  type        = string
-}
